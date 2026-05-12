12a0e63acc942e2163dc3e1ff0c73c5eee82c5c7
Muhamad Arif Rohman <109617046+arifmuhamadrohmann@users.noreply.github.com>
2026-05-13 02:35:01 +0700

    jkse data

--- components/InputPanel.tsx
+++ components/InputPanel.tsx
@@ -17,7 +17,20 @@ type InputPanelProps = {
   onEndDateChange: (date: Date | null) => void;
 };
 
-const MONTHS = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+const MONTHS = [
+  'Januari',
+  'Februari',
+  'Maret',
+  'April',
+  'Mei',
+  'Juni',
+  'Juli',
+  'Agustus',
+  'September',
+  'Oktober',
+  'November',
+  'Desember',
+];
 
 function MonthYearPicker({
   value,
